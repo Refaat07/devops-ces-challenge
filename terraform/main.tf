0d16ec31245e3a72dd3395f7cdfe263e8dc6b233
Mohamed Refaat <m_refaat1612@yahoo.com>
2026-02-12 07:37:24 +0200

    variables cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,21 @@
 module "vpc" {
-  source                  = "./modules/vpc"
-  vpc_cidr_block          = var.vpc_cidr_block
-  snet_cidr_blocks        = var.snet_cidr_blocks
-  snet_availability_zones = var.snet_availability_zones
+  source                         = "./modules/vpc"
+  vpc_cidr_block                 = "10.0.0.0/23"
+  public_snet_cidr_block         = "10.0.0.0/24"
+  private_snet_cidr_block        = "10.0.1.0/24"
+  public_snet_availability_zone  = "eu-central-1a"
+  private_snet_availability_zone = "eu-central-1b"
 }
 
 module "eks" {
-  source               = "./modules/eks"
-  cluster_name         = var.cluster_name
-  cluster_version      = var.cluster_version
-  cluster_role_name    = var.cluster_role_name
-  node_group_role_name = var.node_group_role_name
-  authentication_mode  = var.cluster_authentication_mode
-  subnet_ids           = module.vpc.subnet_ids
-  depends_on           = [module.vpc]
+  source                  = "./modules/eks"
+  cluster_name            = "${var.env_name}-eks"
+  cluster_version         = "1.34"
+  cluster_role_name       = "${var.env_name}-eks-role"
+  node_group_role_name    = "${var.env_name}-node-group-role"
+  authentication_mode     = "API_AND_CONFIG_MAP"
+  subnet_ids              = [module.vpc.public_snet_id, module.vpc.private_snet_id]
+  depends_on              = [module.vpc]
+  endpoint_public_access  = true #TODO: If time allows, set to false and configure access via bastion host or VPN
+  endpoint_private_access = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,68 +1,8 @@
+variable "env_name" {
+  description = "Resouce name prefix for the environment"
+  default     = "dv"
+}
 variable "region" {
   description = "Default Region for AWS resources"
   default     = "eu-central-1"
-}
-
-variable "vpc_cidr_block" {
-  type        = string
-  description = "VPC CIDR block"
-  default     = "10.0.0.0/23"
-}
-
-variable "creds_profile" {
-  description = "AWS credentials profile to use"
-  default     = "default"
-}
-
-variable "snet_cidr_blocks" {
-  type        = list(string)
-  description = "Subnet CIDR blocks"
-  default     = ["10.0.0.0/24", "10.0.1.0/24"]
-}
-
-variable "snet_availability_zones" {
-  type        = list(string)
-  description = "Subnet Availability Zones"
-  default     = ["eu-central-1a", "eu-central-1b"]
-}
-variable "cluster_name" {
-  type        = string
-  description = "EKS Cluster name"
-  default     = "dv-eks"
-}
-
-variable "cluster_authentication_mode" {
-  type        = string
-  description = "EKS Cluster Authentication Mode"
-  default     = "API_AND_CONFIG_MAP"
-}
-
-variable "cluster_version" {
-  type        = string
-  description = "EKS Cluster version"
-  default     = "1.31"
-}
-
-variable "cluster_role_name" {
-  type        = string
-  description = "EKS Role Name"
-  default     = "dv-eks-role"
-}
-
-variable "node_group_role_name" {
-  type        = string
-  description = "EKS Role Name"
-  default     = "dv-eks-node-group-role"
-}
-
-variable "endpoint_public_access" {
-  type        = bool
-  description = "Public access to the EKS cluster endpoint"
-  default     = false
-}
-
-variable "endpoint_private_access" {
-  type        = bool
-  description = "Private access to the EKS cluster endpoint"
-  default     = true
 }
