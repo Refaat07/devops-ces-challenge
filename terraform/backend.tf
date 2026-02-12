094d5300b368004428a57edaabf5c9bb160a1653
Mohamed Refaat <m_refaat1612@yahoo.com>
2026-02-12 07:38:13 +0200

    add remote backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "dv-s3-mrefaat"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
+    bucket       = "dv-s3-mrefaat"
+    key          = "terraform.tfstate"
+    region       = "eu-central-1"
     use_lockfile = true
   }
 }
