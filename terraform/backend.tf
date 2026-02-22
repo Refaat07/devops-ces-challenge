2a6ac33a923289363b35bdd6de11d396c81597bb
Mohamed Refaat <m_refaat1612@yahoo.com>
2026-02-22 21:46:37 +0200

    changed backend bucket name to a variable

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "dv-s3-mrefaat"
+    bucket       = var.s3_bucket_name
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,6 +8,13 @@ variable "region" {
   description = "Default Region for AWS resources"
   default     = "eu-central-1"
 }
+
+variable "s3_bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket for Terraform state"
+  default     = "dv-s3-mrefaat"
+}
+
 variable "oauth2_issuer_url" {
   type        = string
   description = "OAuth2 Issuer URL"
