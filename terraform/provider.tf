18e3b30c5a592db6b0c7c76dddeb1195423eb2d6
Mohamed Refaat <m_refaat1612@yahoo.com>
2026-02-22 19:50:06 +0200

    changed kubernetes authentication method for same-run provisioning

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -21,11 +21,25 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_name]
+  }
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate[0].data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.eks_cluster_name]
+    }
   }
 }
